dd52c8e68d8966c934a934bf6fb54b7f1e5ca81c
Marko Saukko <marko.saukko@jollamobile.com>
2013-08-27 14:43:04 +0000

    [packaging] Add packaging for sbj.
    Signed-off-by: Marko Saukko <marko.saukko@jollamobile.com>

--- rpm/precheckin.sh
+++ rpm/precheckin.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-DEVICES="boston mako"
+DEVICES="boston mako sbj"
 
 for dev in $DEVICES; do
   sed -e "s/@DEVICE@/$dev/g" pulseaudio-modules-droid.spec.in > pulseaudio-modules-droid-$dev.spec
