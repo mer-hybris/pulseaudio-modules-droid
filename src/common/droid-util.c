0adc96eb566a85902bf4aa87ee80839fc60e25da
Juho Hämäläinen <juho.hamalainen@jolla.com>
2022-02-25 13:05:27 +0200

    common: Whitespace fix.

--- src/common/droid-util.c
+++ src/common/droid-util.c
@@ -1515,8 +1515,7 @@ pa_droid_stream *pa_droid_open_output_stream(pa_droid_hw_module *module,
                                              mix_port->flags,
                                              &config_out,
                                              &output->stream,
-                                             device_port->address
-                                             );
+                                             device_port->address);
     pa_droid_hw_module_unlock(module);
 
     if (ret < 0 || !output->stream) {
